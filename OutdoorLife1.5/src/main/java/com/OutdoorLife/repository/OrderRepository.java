bd2c863353755e3b67b2d615409fc8bdf65814ab
Boaz Rogel <124807954+xRoge1x@users.noreply.github.com>
2024-03-03 11:54:55 -0600

    Delete OutdoorLife1.5 directory

--- OutdoorLife1.5/src/main/java/com/OutdoorLife/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.OutdoorLife.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.OutdoorLife.entity.OrderEntity;
-
-public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
-	
-}
