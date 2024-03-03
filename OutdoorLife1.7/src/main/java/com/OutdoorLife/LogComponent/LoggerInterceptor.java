3ff899d396249bda88eba4c9860a85f62c58234f
Boaz Rogel <124807954+xRoge1x@users.noreply.github.com>
2024-03-03 13:45:32 -0600

    Delete OutdoorLife1.7 directory

--- OutdoorLife1.7/src/main/java/com/OutdoorLife/LogComponent/LoggerInterceptor.java
+++
@@ -1,9 +0,0 @@
-package com.OutdoorLife.LogComponent;
-
-import org.slf4j.event.Level;
-
-public interface LoggerInterceptor {
-    void log(Level level, String className, String methodName, String message);
-    
-    
-}
