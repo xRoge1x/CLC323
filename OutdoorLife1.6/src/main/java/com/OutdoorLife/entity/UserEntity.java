8fe18167823778533512e828d3d63e88bab83b7f
xRoge1x <brogel45@gmail.com>
2024-03-03 11:52:33 -0600

    Updated 1.5 and added new color for 1.6

---
+++ OutdoorLife1.6/src/main/java/com/OutdoorLife/entity/UserEntity.java
@@ -0,0 +1,55 @@
+package com.OutdoorLife.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+public class UserEntity {
+    
+    @Id
+    private Long id;
+    
+    @Column("USERNAME")
+    private String username;
+    
+    @Column("EMAIL")
+    private String email;
+    
+    @Column("PASSWORD")
+    private String password;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ OutdoorLife1.6/src/main/java/com/OutdoorLife/model/ProductModel.java
@@ -0,0 +1,48 @@
+package com.OutdoorLife.model;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+public class ProductModel {
+
+	private Long productId;
+
+    private String description;
+
+    @NotBlank(message = "Product name is required")
+    private String productName;
+
+    @Min(value = 0, message = "Price must be a positive number")
+    private int price;
+
+    // Getters and setters
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+    
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
